ed4de2b8f6fb68c82e4f30de83d2053d2a008c97
emanuelanlopes-EVT <emanuelanlopes@gmail.com>
2026-07-09 14:54:08 -0300

    wordmark

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,7 +38,7 @@ const Navbar: React.FC<NavbarProps> = ({ onLogoUpload, logoUrl }) => {
       <nav className="max-w-7xl mx-auto px-6 flex items-center justify-between">
         <a href="#" className="flex items-center gap-2.5 group">
           <img src="/imagens/logo.png.png" alt="Elevarte" className="h-8 w-auto object-contain transition-opacity group-hover:opacity-70" />
-          <span className="text-white font-black text-xl tracking-tight transition-opacity group-hover:opacity-70">ELEVARTE</span>
+          <img src="/imagens/logotipo.png.png" alt="Elevarte" className="h-6 w-auto object-contain transition-opacity group-hover:opacity-70" />
         </a>
 
         {/* Desktop links */}
